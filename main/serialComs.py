e26c0846863d3e0fddfd69ab7a54b9ac343788ca
theotufford <theotufford11@gmail.com>
2025-06-26 13:16:36 -0500

    added untracked files

---
+++ main/serialComs.py
@@ -0,0 +1,36 @@
+import serial
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+ser = serial.Serial("/dev/ttyACM0", 115200, timeout = .5)
+
+testFile = input("input test file name: ")
+f = open(testFile)
+for line in f:
+    doneCount = 0
+    line = line.strip()
+    print(f"line:{line}")
+    offset = 64 - len(line)
+    if offset < 0:
+        print("\n line exceeded buffer!\n")
+    else:
+        for i in range(0, offset):
+            line += "$"
+        print("\nsending line\n")
+        ser.write(line.encode("ascii"))
+    while True:
+        try:
+            readLine = ser.readline().decode("ascii").strip() # decode to string and remove whitespace
+            print(readLine)
+        finally:
+            if "done" in readLine:
+                doneCount += 1
+                if doneCount == 2:
+                    break
+
+
+    time.sleep(.25)
+f.close
+
