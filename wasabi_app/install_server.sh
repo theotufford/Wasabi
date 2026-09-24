034b246577faa0fca7610161db344084410a2005
theo <theotufford11@gmail.com>
2026-09-24 17:29:09 -0400

    updated install script and config constants

--- wasabi_app/install_server.sh
+++ wasabi_app/install_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 apt-get update && \
   apt-get install -y python3 python3-pip nginx curl git
 
@@ -7,3 +8,33 @@ curl -sL https://deb.nodesource.com/setup_20.x | bash - && \
     apt-get install -y nodejs
 
 npm install -g vite
+
+cat << EOF > /etc/systemd/system/wasabi_flask.service
+[Unit]
+Description=wasabi flask backend server and serial server
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=$PWD
+ExecStart=$PWD/start_flask_server.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF > /etc/systemd/system/wasabi_vite.service
+[Unit]
+Description=wasabi interface server startup script
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=$PWD
+ExecStart=$PWD/start_vite_server.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
